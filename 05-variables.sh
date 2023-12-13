b1b69069a955cc15ed3f926678fb710e05e75a3a
Darshan-13197 <darshanvd13197@gmail.com>
2023-12-13 15:30:16 +0530

    Added Second Script

---
+++ 05-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+# The concept is, if w want to connect the External System/Server from the script.
+USERNAME=$1
+PASSWORD=$2
+
+echo "Username is: $USERNAME, Password is: $PASSWORD"
