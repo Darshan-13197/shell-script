433ba302dbbbce99f1fc85c3312888b21503baee
Darshan-13197 <darshanvd13197@gmail.com>
2023-12-13 16:06:07 +0530

    Added Array Script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -3,10 +3,10 @@
 #Declaring the FRUITS 
 FRUITS=("Apple" "Mango" "Banana")
 
-echo "First Value is : ${FRUITS[0]}"
-echo "First Value is : ${FRUITS[1]}"
-echo "First Value is : ${FRUITS[2]}"
+echo "First Value is: ${FRUITS[0]}"
+echo "First Value is: ${FRUITS[1]}"
+echo "First Value is: ${FRUITS[2]}"
 
-echo "ALL FRUITS is : ${FRUITS[@]}"
+echo "ALL FRUITS is: ${FRUITS[@]}"
 
 #In Arrays, the INDEX Value will start from ZERO [0]
