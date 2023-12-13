cbf85e475936f109e8e90332a12bc4045d4b6cb4
Darshan-13197 <darshanvd13197@gmail.com>
2023-12-13 16:01:54 +0530

    Added Array Script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -7,4 +7,6 @@ echo "First Value is : ${FRUITS[0]}"
 echo "First Value is : ${FRUITS[1]}"
 echo "First Value is : ${FRUITS[2]}"
 
+echo "ALL FRUITS is : ${FRUITS[@]}
+
 #In Arrays, the INDEX Value will start from ZERO [0]
