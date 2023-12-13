f72df8742ee21b24a3d5152ebb175e27eb7d4af2
Darshan-13197 <darshanvd13197@gmail.com>
2023-12-13 15:25:38 +0530

    Added Second Script

---
+++ 04-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# Here we passing the Arguments == args which we can control it from Outside.
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2, Good Morning."
+echo "$PERSON2: Hi $PERSON1, Very Good Morning."
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am Good $PERSON1."
+
+# While running the script you need to mention the 2 names as input in the place of $1 and $2
