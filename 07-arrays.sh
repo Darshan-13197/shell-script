4d1aef1e696098f817bd30f552de5a73c98321b2
Darshan-13197 <darshanvd13197@gmail.com>
2023-12-13 15:52:46 +0530

    Added Array Script

---
+++ 07-arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Declaring the FRUITS 
+FRUITS ("Apple" "Mango" "Banana")
+
+echo "First Value is : ${FRUITS[0]}"
+echo "First Value is : ${FRUITS[1]}"
+echo "First Value is : ${FRUITS[2]}"
+
+#In Arrays, the INDEX Value will start from ZERO [0]
