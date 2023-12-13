ccdd3c3fe21fc087f132abed3be27ff09a44c6b9
Darshan-13197 <darshanvd13197@gmail.com>
2023-12-13 13:15:16 +0530

    Added Second Script

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "Ramesh: Hi Suresh, Good Morning."
+echo "Suresh: Hi Ramesh, Very Good Morning."
+echo "Ramesh: How are you Suresh?"
+echo "Suresh: I am Good Suresh."
