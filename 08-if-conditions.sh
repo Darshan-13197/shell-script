70a5f2356db68df81112b7a188db511b874e2dbf
Darshan-13197 <darshanvd13197@gmail.com>
2023-12-14 23:56:00 +0530

    Added if-confition

---
+++ 08-if-conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# gt=greater than, lt=lesser than, ge=greater than equal, lt=lesser than equal
+
+if [ $NUMBER -gt 100 ]
+then
+    echo "Given Number $NUMBER is Greater than 100"
+else
+    echo "Given Number $NUMBER is Not Greater than 100"
+fi 
