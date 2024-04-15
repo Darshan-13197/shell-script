c2a49e52632f6448f10596a9943660a02cf42f6a
Darshan-13197 <darshanvd13197@gmail.com>
2024-04-15 13:31:46 +0530

    added 10th script

--- 10-functions.sh
+++ 10-functions.sh
@@ -4,12 +4,20 @@
 ID=$(id -u)
 
 VALIDATE() {
-    if [ $? -ne 0 ]
-    then
-        echo "Error :: Installing is Failed"
+    # if [ $? -ne 0 ]
+    # then
+    #     echo "Error :: Installing is Failed"
+    #     exit 1
+    # else
+    #     echo "Successfully Installed"
+    # fi
+
+    if [ $1 -ne 0 ]
+    then 
+        echo "Error :: $2 ....is Failed"
         exit 1
     else
-        echo "Successfully Installed"
+        echo "$2 .... is Success"
     fi
 
 }
@@ -22,10 +30,17 @@ else
     echo "You are a root user"
 fi #fi means reveres of if indicating the condition end
 
-# How to call the Function
+# How to call the Function 
 
 yum install mysql -y
-VALIDATE # Here we are calling the function
+#VALIDATE # Here we are calling the function
+
+# How we need to RETRIVE
+
+VALIDATE $? "Installing MySQL" # Giving Some inputs for the Validate Function
+    # Here, we are passing 2 Args in Validate()
+    #     $? --> Exit Status
+    #     "Installing MySQL" --> what I am doing here
 
 yum install git -y 
-VALIDATE
+VALIDATE $? "Installing Git"
