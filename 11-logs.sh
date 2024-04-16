7360d03a86d88cfa1cecfecd18516bc17a1f0c65
Darshan-13197 <darshanvd13197@gmail.com>
2024-04-16 11:10:24 +0530

    added 11th script

---
+++ 11-logs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+## Check user is root or Not
+ID=$(id -u)
+
+# $? --> Exit status of the previos command
+# $0 --> you will get Script name
+
+echo "Script Name: $0"
+
+VALIDATE() {
+
+    if [ $1 -ne 0 ]
+    then 
+        echo "Error :: $2 ....is Failed"
+        exit 1
+    else
+        echo "$2 .... is Success"
+    fi
+
+}
+
+if [ $ID -ne 0 ]  
+then 
+    echo "ERROR::Please run this script with root access."
+    exit 1 # you can give other than 0
+else
+    echo "You are a root user"
+fi #fi means reveres of if indicating the condition end
+
+# How to call the Function and Retrive
+
+yum install mysql -y
+VALIDATE $? "Installing MySQL" # Giving Some inputs for the Validate Function
+
+yum install git -y 
+VALIDATE $? "Installing Git"
