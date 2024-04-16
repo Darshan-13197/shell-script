af0307be3ef3a6d2f51215d50e87df0ad171b5c1
Darshan-13197 <darshanvd13197@gmail.com>
2024-04-16 12:20:47 +0530

    edited 6th script

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -5,3 +5,13 @@ NUMBER2=$2
 
 SUM=$((NUMBER1+NUMBER2))
 echo "Total is: $SUM"
+
+# # --> denotes How many args passed and it will denotes a number.
+echo "How many Args Passed:: $#"
+
+# @ --> provided the All Args
+echo "All args Passed:: $@" 
+
+echo " Script Name : $0"
+
+
