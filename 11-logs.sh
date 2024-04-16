6191b9152c9edccb62baa3a6b9d549e8c16940f9
Darshan-13197 <darshanvd13197@gmail.com>
2024-04-16 12:57:02 +0530

    Edited 11th script

--- 11-logs.sh
+++ 11-logs.sh
@@ -37,10 +37,10 @@ VALIDATE() {
 
 if [ $ID -ne 0 ]  
 then 
-    echo "ERROR::Please run this script with root access."
+    echo -e "$RED ERROR::Please run this script with root access."
     exit 1 # you can give other than 0
 else
-    echo "You are a root user"
+    echo -e "$YELLOW You are a root user"
 fi #fi means reveres of if indicating the condition end
 
 # How to call the Function and Retrive
