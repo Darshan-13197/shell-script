be8be3011d29419a3424c98cf049e10801ca440f
Darshan-13197 <darshanvd13197@gmail.com>
2024-05-06 10:31:02 +0530

    added 9th script

--- 16-ifs.sh
+++ 16-ifs.sh
@@ -15,10 +15,13 @@ then
     echo -e "$R Source directory: $file does not exists. $N"
 fi
 
+#IFS -- Internal Feel Selector
+# The Below paramemters are from the /etc/passwd format.
+
 while IFS=":" read -r username password user_id group_id User_fullname home_Dir shell_path
 do
     echo "username: $username"
     echo "userID: $user_id"
     echo "groupID: $group_id"
-
-done < $file
+    #you can choose other filter
+done < $file # Input is $file
---
+++ 17-disk-usage.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
+    #here E is an Expression, | is OR, here you do not want these lines.
+
+DISK_THRESHOLD=1 # Usually we give 70% but our Disk is not used more than 70.
+message=""
+
+while IFS=readline
+do
+    #Here we are printing only the 6th Fragment and removing the % and Last Fragement
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+
+    #Also we need Disk Partition
+    partition=$(echo $line | awk '{print $1F}')
+
+    #Condition is usage should be greather than Threshold
+    if [$usage -ge $DISK_THRESHOLD]
+    then
+        message+="High Disk Usage on $partition: $usage\n" 
+        #+= --> It will add Multiple Entries without overwrite. you can remove + and check
+    fi
+done <<< DISK_USAGE #<<< --> This is for Command output.
+echo -e "Message:$message" # we are enabling the special character.
