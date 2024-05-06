22d079fab396e3c975bf244feae8d1d01dbf8558
Darshan-13197 <darshanvd13197@gmail.com>
2024-05-06 13:18:21 +0530

    added 9th script

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -3,10 +3,11 @@
 DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
     #here E is an Expression, | is OR, here you do not want these lines.
 
-DISK_THRESHOLD=1 # Usually we give 70% but our Disk is not used more than 70.
+DISK_THRESHOLD=1
+# Usually we give 70% but our Disk is not used more than 70.
 message=""
 
-while IFS=readline
+while IFS=read line
 do
     #Here we are printing only the 6th Fragment and removing the % and Last Fragement
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
@@ -15,7 +16,7 @@ do
     partition=$(echo $line | awk '{print $1F}')
 
     #Condition is usage should be greather than Threshold
-    if [ $usage -ge: $DISK_THRESHOLD ]
+    if [ $usage -ge $DISK_THRESHOLD ]
     then
         message+="High Disk Usage on $partition: $usage\n" 
         #+= --> It will add Multiple Entries without overwrite. you can remove + and check
