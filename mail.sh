b0cca05acf4c9ec8e2cdd55916802a2497342d1a
Darshan-13197 <darshanvd13197@gmail.com>
2024-05-06 14:00:10 +0530

    added 17th script

--- mail.sh
+++ mail.sh
@@ -10,5 +10,5 @@ SUBJECT=$5
 FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/BODY/$ESCAPE_BODY/g" template.html)
 #using -e for temporary not permament, other team cannot use thi if we choose permanent(-i)
 
-echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT" "$TO_ADDRESS"
+echo "$FINAL_BODY" | mail -s "$SUBJECT" "$TO_ADDRESS"
 #echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
