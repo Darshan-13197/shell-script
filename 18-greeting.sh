d51e14fc9201638dea5f10854460ec7cae19720b
Darshan-13197 <darshanvd13197@gmail.com>
2024-05-07 14:37:14 +0530

    added 9th script

--- 18-greeting.sh
+++ 18-greeting.sh
@@ -25,9 +25,9 @@ done
 
 #We are writing some Validation for the OPTIONS.
 
-if [ -z "$NAME" ] || [ -z "$WISHES" ]; then # if both are empty will show ERROR
-#if [ -z "$NAME" ]; then #Now wishes is optional
-    echo "ERROR: Both -n and -w are Mandatory Options"
+#if [ -z "$NAME" ] || [ -z "$WISHES" ]; then # if both are empty will show ERROR
+if [ -z "$NAME" ]; then #Now wishes is optional
+    #echo "ERROR: Both -n and -w are Mandatory Options"
     echo "ERROR: -n is Mandatory"
     USAGE
     exit 1   
