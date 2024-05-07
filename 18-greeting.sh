4aed5668d5641aa6c4da9b14a82cdbc6969b4aca
Darshan-13197 <darshanvd13197@gmail.com>
2024-05-07 14:23:44 +0530

    added 9th script

--- 18-greeting.sh
+++ 18-greeting.sh
@@ -15,8 +15,10 @@ USAGE() { #Here we are giving the info about OPTIONS
 while getopts "n:w:h" opt; do
 #using case 
     case $opt in # For OPTIONS, there is no sequence here
-        n) NAME=$OPTARGS;; # Special Variables
-        w) WISHES=$OPTARGS;;
-        h|*) USAGE; exit;; #calling usage function
+        n) NAME=$OPTARG;; # Special Variables
+        w) WISHES=$OPTARG;;
+        \?) echo "Invalid Option: -"$OPTARG"" >&2; USAGE; exit;;
+        :) USAGE; exit;;   
+        h) USAGE; exit;; #calling usage function
     esac
 done
